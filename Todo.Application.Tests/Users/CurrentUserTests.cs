0f04a9b79a076e541b1c5bc94167e7ebd3a12796
Nicolas <nico.daddabbo7100@gmail.com>
2024-06-28 14:57:44 +0200

    Début tests unitaires

---
+++ Todo.Application.Tests/Users/CurrentUserTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Todo.Domain.Constants;
+using Xunit;
+
+namespace Todo.Application.Users.Tests;
+
+public class CurrentUserTests
+{
+    [Fact()]
+    public void HasRole_WithMatchingRole_ShouldReturnTrue()
+    {
+        // arrange
+
+        var currentUser = new CurrentUser("1", "test@gmail.com", UserRole.SuperAdmin); 
+
+        // act
+
+        var hasRole = currentUser.HasRole(UserRole.SuperAdmin);
+
+        // assert
+
+        hasRole.Should().BeTrue();
+    }
+
+    [Theory()]
+    [InlineData(UserRole.User)]
+    [InlineData(UserRole.Admin)]
+    [InlineData(UserRole.Leader)]
+    public void HasRole_WithNoMatchingRole_ShouldReturnFalse(string role)
+    {
+        // arrange
+
+        var currentUser = new CurrentUser("1", "test@gmail.com", UserRole.SuperAdmin);
+
+        // act
+
+        var hasRole = currentUser.HasRole(role);
+
+        // assert
+
+        hasRole.Should().BeFalse();
+    }
+
+}
