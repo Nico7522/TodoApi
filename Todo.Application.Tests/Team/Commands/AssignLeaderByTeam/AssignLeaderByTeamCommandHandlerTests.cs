619abb862329291ba988b539fee9091f4f391815
Nicolas <nico.daddabbo7100@gmail.com>
2024-07-04 13:24:34 +0200

    Début des tests pour les commands team

---
+++ Todo.Application.Tests/Team/Commands/AssignLeaderByTeam/AssignLeaderByTeamCommandHandlerTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Todo.Domain.Entities;
+using Todo.Domain.Exceptions;
+using Todo.Domain.Repositories;
+using Xunit;
+using TaskAsync = System.Threading.Tasks.Task;
+namespace Todo.Application.Team.Commands.AssignLeader.Tests;
+
+public class AssignLeaderByTeamCommandHandlerTests
+{
+    private readonly Mock<ITeamRepository> _teamRepositoryMock;
+    private readonly Mock<UserManager<UserEntity>> _userManagerMock;
+    private readonly Guid _teamId;
+    private readonly string _leaderId;
+    private readonly AssignLeaderByTeamCommandHandler _handler;
+    public AssignLeaderByTeamCommandHandlerTests()
+    {
+        _teamRepositoryMock = new Mock<ITeamRepository>();
+        _userManagerMock = new Mock<UserManager<UserEntity>>(new Mock<IUserStore<UserEntity>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+    new Mock<IPasswordHasher<UserEntity>>().Object,
+    new IUserValidator<UserEntity>[0],
+    new IPasswordValidator<UserEntity>[0],
+    new Mock<ILookupNormalizer>().Object,
+    new Mock<IdentityErrorDescriber>().Object,
+    new Mock<IServiceProvider>().Object,
+    new Mock<ILogger<UserManager<UserEntity>>>().Object
+            );
+        _teamId = Guid.NewGuid();
+        _leaderId = "leaderId";
+        _handler = new AssignLeaderByTeamCommandHandler(_teamRepositoryMock.Object, _userManagerMock.Object);
+    }
+
+    [Fact()]
+    public async TaskAsync Handle_ForValidCommand_ShouldAssignLeaderAndAddUserToTeam()
+    {
+        // arrange
+
+        var team = new TeamEntity()
+        {
+            Id = _teamId,
+            LeaderId = null,
+            Users = new List<UserEntity>()
+        };
+
+        var user = new UserEntity() { Id = _leaderId, TeamId = null };
+
+        _teamRepositoryMock.Setup(r => r.GetById(team.Id)).ReturnsAsync(team);
+        _userManagerMock.Setup(m => m.FindByIdAsync(_leaderId)).ReturnsAsync(user);
+        var command = new AssignLeaderByTeamCommand(_teamId, _leaderId);
+
+        // act
+
+        await _handler.Handle(command, CancellationToken.None);
+
+        // assert
+
+        _teamRepositoryMock.Verify(r => r.GetById(_teamId), Times.Once());
+        _userManagerMock.Verify(m => m.FindByIdAsync(user.Id), Times.Once());
+        team.LeaderId.Should().Be(user.Id);
+        team.Users.Should().HaveCount(1);
+        team.Users.First().Id.Should().Be(user.Id);
+    }
+
+    [Fact()]
+    public async TaskAsync Handle_ForNoExistingUser_ShouldThrowNotFoundException()
+    {
+        // arrange
+
+        var team = new TeamEntity()
+        {
+            Id = _teamId,
+            LeaderId = null,
+            Users = new List<UserEntity>()
+        };
+
+        var user = new UserEntity() { Id = "id", TeamId = null };
+
+        _teamRepositoryMock.Setup(r => r.GetById(team.Id)).ReturnsAsync(team);
+        _userManagerMock.Setup(m => m.FindByIdAsync(_leaderId)).ReturnsAsync((UserEntity?)null);
+        var command = new AssignLeaderByTeamCommand(_teamId, _leaderId);
+
+        // act
+
+        Func<TaskAsync> act = async () => await _handler.Handle(command, CancellationToken.None);
+
+        // assert
+
+        _teamRepositoryMock.Verify(r => r.GetById(_teamId), Times.Never());
+        _userManagerMock.Verify(m => m.FindByIdAsync(user.Id), Times.Never());
+        await act.Should().ThrowAsync<NotFoundException>().WithMessage("User not Found");
+        team.LeaderId.Should().Be(null);
+        team.Users.Should().BeEmpty();
+        user.TeamId.Should().Be(null);  
+    }
+}
--- Todo.Application/Team/Commands/AssignLeaderByTeam/AssignLeaderByTeamCommandHandler.cs
+++ Todo.Application/Team/Commands/AssignLeaderByTeam/AssignLeaderByTeamCommandHandler.cs
@@ -1,53 +1,41 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Todo.Application.Users;
 using Todo.Domain.Entities;
 using Todo.Domain.Exceptions;
 using Todo.Domain.Repositories;
 
-namespace Todo.Application.Team.Commands.AssignLeader
+namespace Todo.Application.Team.Commands.AssignLeader;
+public class AssignLeaderByTeamCommandHandler : IRequestHandler<AssignLeaderByTeamCommand>
 {
+    private readonly ITeamRepository _teamRepository;
+    private readonly UserManager<UserEntity> _userManager;
 
-
-    internal class AssignLeaderByTeamCommandHandler : IRequestHandler<AssignLeaderByTeamCommand>
+    public AssignLeaderByTeamCommandHandler(
+        ITeamRepository teamRepository, 
+        UserManager<UserEntity> userManager
+       )
+    {
+        _teamRepository = teamRepository;
+        _userManager = userManager;
+    }
+    public async System.Threading.Tasks.Task Handle(AssignLeaderByTeamCommand request, CancellationToken cancellationToken)
     {
 
-        private readonly ITeamRepository _teamRepository;
-        private readonly UserManager<UserEntity> _userManager;
+        var team = await _teamRepository.GetById(request.TeamId);
+        if (team is null) throw new NotFoundException("Team not found");
 
-        public AssignLeaderByTeamCommandHandler(
-            ITeamRepository teamRepository, 
-            UserManager<UserEntity> userManager
-           )
-        {
-            _teamRepository = teamRepository;
-            _userManager = userManager;
-        }
-        public async System.Threading.Tasks.Task Handle(AssignLeaderByTeamCommand request, CancellationToken cancellationToken)
-        {
+        var user = await _userManager.FindByIdAsync(request.UserId);
+        if (user is null) throw new NotFoundException("User not found");
 
-            var team = await _teamRepository.GetById(request.TeamId);
-            if (team is null) throw new NotFoundException("Team not found");
+        if (team.LeaderId == user.Id) throw new BadRequestException("User is already leader of this team");
 
-            var user = await _userManager.FindByIdAsync(request.UserId);
-            if (user is null) throw new NotFoundException("User not found");
+        if (user.TeamId != null) throw new BadRequestException("User is already in a team");
 
-            if (team.LeaderId == user.Id) throw new BadRequestException("User is already leader of this team");
+        if (team.LeaderId != null) throw new BadRequestException("Team has already a leader");
 
-            if (user.TeamId != null) throw new BadRequestException("User is already in a team");
+        team.LeaderId = user.Id;
+        if(user.TeamId != team.Id) team.Users.Add(user);
 
-            if (team.LeaderId != null) throw new BadRequestException("Team has already a leader");
-
-            team.LeaderId = user.Id;
-            if(user.TeamId != team.Id) team.Users.Add(user);
-
-            await _teamRepository.SaveChanges();
-        }
+        await _teamRepository.SaveChanges();
     }
 }
